0ae20544c7427f0d23bcce6b7ddf52d1a97e08ca
gagandeepte <gagandeep@thirdeyedata.io>
2021-02-22 11:02:32 +0530

    First commit

---
+++ src/Room.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+import Video from "twilio-video";
+import Participant from "./Participant";
+
+const Room = ({ roomName, token, handleLogout }) => {
+  const [room, setRoom] = useState(null);
+  const [participants, setParticipants] = useState([]);
+  const [toggleAudio, setToggleAudio] = useState(true);
+  const [toggleVideo, setToggleVideo] = useState(true);
+
+  useEffect(() => {
+    const participantConnected = (participant) => {
+      setParticipants((prevParticipants) => [...prevParticipants, participant]);
+    };
+
+    const participantDisconnected = (participant) => {
+      setParticipants((prevParticipants) =>
+        prevParticipants.filter((p) => p !== participant)
+      );
+    };
+
+    
+    Video.connect(token, {
+      name: roomName
+    }).then(room => {
+      setRoom(room);
+      room.on("participantConnected", participantConnected);
+      room.on("participantDisconnected", participantDisconnected);
+      room.participants.forEach(participantConnected);
+    });
+    return () => {
+      setRoom(currentRoom => {
+        if (currentRoom && currentRoom.localParticipant.state === "connected") {
+          currentRoom.localParticipant.tracks.forEach(function(
+            trackPublication
+          ) {
+            trackPublication.track.stop();
+          });
+          currentRoom.disconnect();
+          return null;
+        } else {
+          return currentRoom;
+        }
+      });
+    };
+  }, [roomName,token]);
+
+  const handleCallDisconnect = () => {
+    room.disconnect();
+  };
+
+  const handleAudioToggle = () => {
+    room.localParticipant.audioTracks.forEach(track => {
+      if (track.track.isEnabled) {
+        track.track.disable();
+      } else {
+        track.track.enable();
+      }
+      setToggleAudio(track.track.isEnabled);
+    });
+  };
+
+  const handleVideoToggle = () => {
+    room.localParticipant.videoTracks.forEach(track => {
+      if (track.track.isEnabled) {
+        track.track.disable();
+      } else {
+        track.track.enable();
+      }
+      setToggleVideo(track.track.isEnabled);
+    });
+  };
+
+  const remoteParticipants = participants.map((participant) => (
+    <Participant
+      key={participant.sid}
+      participant={participant}
+      isLocal={false}
+    />
+  ));
+
+  return (
+    <div className="room">
+      <h2>Room: {roomName}</h2>
+      <button onClick={handleLogout}>Log out</button>
+      <div className="local-participant">
+        {room ? (
+          <Participant
+            key={room.localParticipant.sid}
+            participant={room.localParticipant}
+            handleAudioToggle={handleAudioToggle}
+            handleVideoToggle={handleVideoToggle}
+            handleCallDisconnect={handleCallDisconnect}
+            toggleAudio={toggleAudio}
+            toggleVideo={toggleVideo}
+            isLocal={true}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+      <h3>Remote Participants</h3>
+      <div className="remote-participants">{remoteParticipants}</div>
+    </div>
+  );
+};
+
+export default Room;
---
+++ src/VideoChat.js
@@ -0,0 +1,84 @@
+import React, { useState, useCallback, useEffect } from "react";
+import Video from "twilio-video";
+import Lobby from "./Lobby";
+import Room from "./Room";
+
+const VideoChat = () => {
+  const [username, setUsername] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [room, setRoom] = useState(null);
+  const [connecting, setConnecting] = useState(false);
+  const [token, setToken] = useState(null);
+
+  const handleUsernameChange = useCallback((event) => {
+    setUsername(event.target.value);
+  }, []);
+
+  const handleRoomNameChange = useCallback((event) => {
+    setRoomName(event.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+      setConnecting(true);
+      const data = await fetch("/video/token", {
+        method: "POST",
+        body: JSON.stringify({
+          identity: username,
+          room: roomName,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+      setConnecting(false);
+      setToken(data.token);
+    },
+    [roomName, username]
+  );
+
+  const handleLogout = useCallback(() => {
+    setToken(null)
+  }, []);
+
+  useEffect(() => {
+    if (room) {
+      const tidyUp = (event) => {
+        if (event.persisted) {
+          return;
+        }
+        if (room) {
+          handleLogout();
+        }
+      };
+      window.addEventListener("pagehide", tidyUp);
+      window.addEventListener("beforeunload", tidyUp);
+      return () => {
+        window.removeEventListener("pagehide", tidyUp);
+        window.removeEventListener("beforeunload", tidyUp);
+      };
+    }
+  }, [room, handleLogout]);
+
+  let render;
+  if (token) {
+    render = (
+      <Room roomName={roomName} token={token} handleLogout={handleLogout} />
+    );
+  } else {
+    render = (
+      <Lobby
+        username={username}
+        roomName={roomName}
+        handleUsernameChange={handleUsernameChange}
+        handleRoomNameChange={handleRoomNameChange}
+        handleSubmit={handleSubmit}
+        connecting={connecting}
+      />
+    );
+  }
+  return render;
+};
+
+export default VideoChat;
